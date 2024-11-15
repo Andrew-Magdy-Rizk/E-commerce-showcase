423e5c2141148f3876d7e9eea47f055c8501e9c2
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2024-11-15 18:40:18 +0200

    edit provider1

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${roboto.variable} antialiased`}>
-        <ProviderWrap children={children}>
+        <ProviderWrap>
           <Header />
           {children}
           <Footer />
