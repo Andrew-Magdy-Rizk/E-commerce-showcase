f0b68ade220965cbd6eac54d2f5cb44350a969f8
Andrew Magdy <89481184+Andrew-Magdy-Rizk@users.noreply.github.com>
2024-11-17 06:39:40 +0200

    update 7

--- app/_Components/ProductsSection.jsx
+++ app/_Components/ProductsSection.jsx
@@ -40,7 +40,7 @@ function ProductsSection() {
     <div className='w-full dark:bg-gray-900'>
     <div className='container py-16 mx-auto px-5'>
         <div className='text-center mb-8'>
-            <h2 className='uppercase md:text-5xl text-4xl font-bold tracking-wide mb-2 dark:text-white' id='products'>COLLECTION{stateProducts.length}</h2>
+            <h2 className='uppercase md:text-5xl text-4xl font-bold tracking-wide mb-2 dark:text-white' id='products'>COLLECTION</h2>
             <p className='md:text-sm text-xs text-gray-600 font-medium dark:text-gray-300'>Donec venenatis vulputate lorem roin sapien ipsum, porta a auctor</p>
         </div>
         <div className='flex flex-col md:flex-row justify-start items-center mb-10'>
